219173e1b9ba3c4e14b23918ce13d4212a4411b9
Tom <thomas.spencer@codurance.com>
2021-08-04 22:10:41 +0100

    add grid and movement

--- test/Rover.test.ts
+++ test/Rover.test.ts
@@ -1,8 +1,16 @@
 import Rover from "../src/Rover";
-
-const rover: Rover = new Rover();
+import Grid from "../src/Grid";
+import Coordinate from "../src/Coordinate";
 
 describe("Rover should", () => {
+  let rover: Rover;
+
+  beforeEach(() => {
+    const coordinate: Coordinate = new Coordinate(0, 0);
+    const grid: Grid = new Grid(coordinate);
+    rover = new Rover(grid);
+  });
+
   test("return current coordinates", () => {
     expect(rover.execute("")).toEqual("0:0:N");
   });
@@ -16,6 +24,8 @@ describe("Rover should", () => {
     ["LL", "0:0:S"],
     ["LLL", "0:0:E"],
     ["LLLL", "0:0:N"],
+    ["RM", "1:0:E"],
+    ["RRM", "0:9:S"],
   ])(`input command of %p returns %p`, (command: string, result: string) => {
     expect(rover.execute(command)).toEqual(result);
   });
